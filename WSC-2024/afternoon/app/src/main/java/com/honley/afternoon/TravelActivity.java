08dace95a57f677e95d838f1cf198fe6e3fb86eb
honley1 <mfhonley@gmail.com>
2024-11-29 10:41:38 +0500

    add travel activity functionaltiy

--- WSC-2024/afternoon/app/src/main/java/com/honley/afternoon/TravelActivity.java
+++ WSC-2024/afternoon/app/src/main/java/com/honley/afternoon/TravelActivity.java
@@ -1,5 +1,6 @@
 package com.honley.afternoon;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,6 +9,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -43,17 +45,66 @@ public class TravelActivity extends AppCompatActivity {
             startActivity(new Intent(TravelActivity.this, HomeActivity.class));
         });
 
-        binding.arcdetriophe.setOnClickListener(v -> {
-            String place = binding.arcdetriophe.toString();
-            dialog(place);
+        binding.toureiffel.setOnClickListener(v -> {
+            String place = "toureiffel";
             System.out.println(place);
+            dialog(place);
+        });
+
+        binding.socrecoeur.setOnClickListener(v -> {
+            String place = "socrecoeur";
+            System.out.println(place);
+            dialog(place);
+        });
+
+        binding.arcdetriophe.setOnClickListener(v -> {
+            String place = "arcdetriophe";
+            System.out.println(place);
+            dialog(place);
+        });
+
+        binding.palaisRoyal.setOnClickListener(v -> {
+            String place = "palaisRoyal";
+            System.out.println(place);
+            dialog(place);
         });
     }
 
+    @SuppressLint("ResourceType")
     private void dialog(String place) {
         Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.place);
+        dialog.setContentView(R.layout.dialog_place);
         dialog.setCancelable(true);
+
+        ImageView placeImage = dialog.findViewById(R.id.placeImgae);
+        TextView placeText = dialog.findViewById(R.id.placeText);
+
+        switch (place) {
+            case "toureiffel":
+                placeImage.setImageResource(R.drawable.scene_1); // Устанавливаем изображение
+                placeText.setText(R.string.toureiffel);          // Устанавливаем текст
+                break;
+
+            case "socrecoeur":
+                placeImage.setImageResource(R.drawable.scene_5);
+                placeText.setText(R.string.socrecoeur);
+                break;
+
+            case "arcdetriophe":
+                placeImage.setImageResource(R.drawable.scene_2);
+                placeText.setText(R.string.arcdetriophe);
+                break;
+
+            case "palaisRoyal":
+                placeImage.setImageResource(R.drawable.scene_4);
+                placeText.setText(R.string.palaisRoyal);
+                break;
+
+            default:
+                placeImage.setImageResource(R.drawable.icon_close);
+                placeText.setText(R.string.content);
+                break;
+        }
 
         dialog.show();
     }
